9871630cde506d07d9a914c250e5ff7d4d3a9900
Erinchampwalker <87877090+Erinchampwalker@users.noreply.github.com>
2021-09-20 12:18:26 +0200

    last

--- js/app.js
+++ js/app.js
@@ -1,2 +1,28 @@
+
+
+
+
+/* CUSTOM SEARCH 😮 */
+
+
+let userInput = document.querySelector('.searchbox');
+let a = document.querySelectorAll('a');
+let imageArray = Array.from(a);
+
+const searchFilter = () => {
+  for (i=0; i<imageArray.length; i++){
+    let dataCaption = imageArray[i].getAttribute('data-caption').toLowerCase();
+
+
+    if (dataCaption.includes(userInput.value.toLowerCase())){
+      imageArray[i].style.display = 'block';
+    } else {
+      imageArray[i].style.display = 'none';
+    }
+  }
+};
+
+
+
 baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');
+
